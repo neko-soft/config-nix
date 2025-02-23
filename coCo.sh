dfb4205ca66c27680c28caa2b2f2e891b41d7c8e
neko-soft <rodrigo.pereira.riquelme@gmail.com>
2025-02-23 15:46:32 -0300

    Backup automático de dotfiles

---
+++ coCo.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# Lista de carpetas y archivos a respaldar
+CONFIG_DIRS=(
+    "$HOME/.config/hypr"
+    "$HOME/.config/rofi"
+    "$HOME/.config/kitty"
+    "$HOME/.config/waybar"
+    "$HOME/.config/swaylock"
+    "/etc/nixos"
+)
+
+CONFIG_FILES=(
+    "$HOME/.bashrc"
+)
+
+# Carpeta destino (tu repo de dotfiles)
+DEST="$HOME/config-nix"
+
+echo "📂 Copiando configuraciones a $DEST ..."
+
+# Copiar carpetas completas
+for DIR in "${CONFIG_DIRS[@]}"; do
+    if [ -d "$DIR" ]; then
+        cp -r "$DIR" "$DEST"
+        echo "✅ Carpeta copiada: $DIR"
+    else
+        echo "❌ Carpeta no encontrada: $DIR"
+    fi
+done
+
+rm -rf $DEST/kitty/kitty-themes
+rm $DEST/nixos/hardware-configuration.nix
+
+# Copiar archivos individuales
+for FILE in "${CONFIG_FILES[@]}"; do
+    if [ -f "$FILE" ]; then
+        cp "$FILE" "$DEST"
+        echo "✅ Archivo copiado: $FILE"
+    else
+        echo "❌ Archivo no encontrado: $FILE"
+    fi
+done
+
+echo "✅ Copia completada."
+
+# Subir cambios a GitHub
+cd "$DEST"
+git add .
+git commit -m "Backup automático de dotfiles"
+git push origin main
+
+#echo "🚀 Dotfiles subidos a GitHub."
