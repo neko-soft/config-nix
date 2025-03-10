4cb39b559603fcd9c61439a012ab19e1b7799b32
neko-soft <rodrigo.pereira.riquelme@gmail.com>
2025-03-10 14:37:53 -0300

    Copia automática de dotfiles

--- refresco.sh
+++ refresco.sh
@@ -2,7 +2,7 @@
 
 # La idea de este script es que actualice todas las cosas de hyprland, hyprpaper cada vez que se reconstruya el sistema. La idea es dejarlo como un servicio que se ejecute constantemente cuando se reconstruya todo.
 
-hyprctl reload
+/run/current-system/sw/bin/hyprctl reload
 pkill hyprpaper
 
 # Esto es porque el pkill se demora un poco en matar el hyprpaper, así que espera hasta que realmente esté muerto antes de seguir, porque si no, te va a tirar un error que dice que no puedes tener varias instancias de hyprpaper al mismo tiempo.4
